fc0275551815dc5723363ace02b3accb6ad8817e
Shishupal <shishupal614@gmail.com>
2020-06-26 13:38:33 +0530

    codes

---
+++ index.php
@@ -0,0 +1,10 @@
+<html> 
+    <body>
+      <?php
+        $color[A[D[D[D[A[D[D bgcolor="aqua">[B[B[D[D[D[D[D[D[D[D[D[D[D="red";
+        echo"DevOps Assembly Line Task Completed".$color."</br>";
+        print `ifconfig`
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+      ?>
+   </body>
+</html>
